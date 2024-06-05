8e882775f1a0a7eb7bb1902466d5f67c53b84be2
Emad Iddin Ali <ea224pn@student.lnu.se>
2024-06-05 19:10:07 +0200

    Add project files.

---
+++ array uppgift2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Inlamning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många heltal vill du skriva in?");
+            int antal = int.Parse(Console.ReadLine());
+            int[] tal = new int[antal];
+            Console.WriteLine($"Skriv in {antal} heltal");
+            for (int i = 0; i < antal; i++)
+            {
+                tal[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Här är talen du skrev in");
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine($"Tal {i + 1}: {tal[i]}");
+            }
+        }
+    }
+}
